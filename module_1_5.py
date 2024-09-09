d2f810c751b69511dabdb4d42a1173042c47a2af
Phil-Kostrin <kostrin.f@mail.ru>
2024-09-09 18:28:20 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,14 @@
+immutable_var = 27, False, 'Аквариум', 15.3
+print(immutable_var)
+# immutable_var[0] = 'Urban'
+# print(immutable_var)
+# Изменение значения переменных кортежа невозможно, т.к. кортеж не поддерживает обращение к переменным,
+# но если значение переменной в котреже записано в виде списка, то мы можем заменять значения переменных в этом списке.
+mutable_list = [66, 'Fish', True, 12532, 864.2, 'Море']
+print(mutable_list)
+mutable_list[1] = 'Fisherman'
+print(mutable_list)
+mutable_list[5] = 'Sun'
+print(mutable_list)
+mutable_list[3] = 'Число'
+print(mutable_list)
